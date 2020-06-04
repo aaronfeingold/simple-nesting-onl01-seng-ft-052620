04f4ba9aa62a5e732c2cb410205525ab800e73f2
Aaron Feingold <ajfeingold88@gmail.com>
2020-06-04 02:31:44 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -78,11 +78,12 @@ def adding_matz
         }
      }
      
-  programmer_hash << {:yukihiro_matsumoto => {
+  programmer_hash << ({:yukihiro_matsumoto => {
    :known_for => "Ruby",
    :languages => ["LISP", "C"]
       }
     }
+   )
     
 end
 
